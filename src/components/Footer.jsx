62c36734d5fb4e59d048c4c6ed5da0213622120c
RAUSHANARYA <raushankumarlnct07@gmail.com>
2026-06-16 19:27:38 +0530

    client

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="bg-black text-white p-4 text-center">
-      © 2026 All Rights Reserved
+      © 2026 All Rights Reserved Done
     </footer>
   );
 };
